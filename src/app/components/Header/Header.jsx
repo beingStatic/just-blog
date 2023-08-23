62718789592f98065a53750925681d10fe68dde3
beingStatic <mohammedsufiyan23@gmail.com>
2023-08-23 19:57:58 +0530

    Few tweeks

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,21 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
-const headerData = [{
-  id: '1',
-  title: 'Home',
-  url:'/'
-}, {
-  id: '2',
-  title: 'About',
-  url:'/about'
-}]
+import React from 'react';
+import Link from 'next/link';
+const headerData = [
+  {
+    id: '1',
+    title: 'Home',
+    url: '/',
+  },
+  {
+    id: '2',
+    title: 'About',
+    url: '/about',
+  },
+];
 
 const Header = () => {
   return (
-    <div>{headerData.map(item => {
-      return <Link key={item.id} url={item.url} className='cursor-pointer'>{item.title}</Link>
-    })}</div>
-  )
-}
+    <div>
+      {headerData.map((item) => {
+        return (
+          <Link key={item.id} className="cursor-pointer" href={item.url}>
+            {item.title}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Header
+export default Header;
