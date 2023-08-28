d04f38c6e97bfb17344089350b447817a8257d6e
beingStatic <mohammedsufiyan23@gmail.com>
2023-08-28 19:43:37 +0530

    just few tweeks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,10 @@ import HeroImg from '../../public/imgs/home-hero-image.jpg';
 export default function Home() {
   return (
     <main>
-      <section>
-        <div></div>
+      <section className="grid grid-cols-2 h-[100vh]">
+        <div>
+          <h1 className="text-5xl font-bold">Just Blogs</h1>
+        </div>
         <div>
           <Image src={HeroImg} alt="hero Img" />
         </div>
